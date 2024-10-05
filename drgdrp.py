2875926f34d60c2e45aa247188f65f532461d6e4
Raju S Baradur <125146841+RAJUS248@users.noreply.github.com>
2024-10-05 18:00:07 +0530

    Update drgdrp.py

--- drgdrp.py
+++ drgdrp.py
@@ -1,1 +1,79 @@
+import cv2
+import cvzone
+import numpy as np
+from cvzone.HandTrackingModule import HandDetector
+
+# Initialize the webcam
+cap = cv2.VideoCapture(0)
+cap.set(3, 1280)  # Width
+cap.set(4, 720)  # Height
+
+# Initialize the hand detector
+detector = HandDetector(detectionCon=0.8)
+colorR = (255, 0, 0)
+
+cx, cy, w, h = 100, 100, 200, 200
+
+
+class DragRect():
+    def __init__(self, posCenter, size=[200, 200]):
+        self.posCenter = posCenter
+        self.size = size
+
+    def update(self, cursor):
+        cx, cy = self.posCenter
+        w, h = self.size
+
+        # If the index finger tip is in the rectangle region
+        if cx - w // 2 < cursor[0] < cx + w // 2 and \
+                cy - h // 2 < cursor[1] < cy + h // 2:
+            self.posCenter = cursor
+
+
+rectList = []
+for x in range(5):
+    rectList.append(DragRect([x * 250 + 150, 150]))
+
+while True:
+    success, img = cap.read()
+    img = cv2.flip(img, 1)
+
+    # Detect hands
+    hands, img = detector.findHands(img)
+    if hands:
+        hand = hands[0]  # Assume only one hand for simplicity
+        lmList = hand['lmList']  # List of 21 landmark points
+        if lmList:
+            # Extract (x, y) coordinates
+            x1, y1 = lmList[8][0], lmList[8][1]
+            x2, y2 = lmList[12][0], lmList[12][1]
+
+            # Calculate the distance between the tips of the index and middle fingers
+            length, info, img = detector.findDistance((x1, y1), (x2, y2), img)
+            if length < 50:
+                cursor = (x1, y1)  # Index finger tip coordinates
+                # Call the update here
+                for rect in rectList:
+                    rect.update(cursor)
+
+    # Draw with transparency
+    imgNew = np.zeros_like(img, np.uint8)
+    for rect in rectList:
+        cx, cy = rect.posCenter
+        w, h = rect.size
+
+        cv2.rectangle(imgNew, (cx - w // 2, cy - h // 2), (cx + w // 2, cy + h // 2), colorR, cv2.FILLED)
+        cvzone.cornerRect(imgNew, (cx - w // 2, cy - h // 2, w, h), 20, rt=0)
+
+    out = img.copy()
+    alpha = 0.2
+    mask = imgNew.astype(bool)
+    out[mask] = cv2.addWeighted(img, alpha, imgNew, 1 - alpha, 0)[mask]
+
+    cv2.imshow("Image", out)
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+
+cap.release()
+cv2.destroyAllWindows()
 
